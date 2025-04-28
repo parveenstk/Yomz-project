b3270b278c37a984bcfb1116160f5da0ff7b3df6
parveenstk <parveen.kumar@suretekinfosoft.com>
2025-04-28 11:42:34 +0530

    some changesare done.

---
+++ Script/index.js
@@ -0,0 +1,23 @@
+// Links in the variable
+const links = {
+    // Header
+    "home-header": "https://suretekinfosoft.com/demo106/yomz-storefront-home/",
+    "about-header": "https://suretekinfosoft.com/demo106/yomz/pages/yomz-story/",
+    "oggummies-header": "https://suretekinfosoft.com/demo106/products/yomz-OG-gummies/",
+    "sours-header": "https://suretekinfosoft.com/demo106/products/yomz-sours/",
+
+    // try yomz buttons
+    "ogGummiesProduct": "https://suretekinfosoft.com/demo106/products/yomz-OG-gummies/",
+    "soursProduct": "https://suretekinfosoft.com/demo106/products/yomz-sours/",
+
+    // logo
+    "yomzLogo": "https://suretekinfosoft.com/demo106/yomz-storefront-home/",
+    "yomzLogo2": "https://suretekinfosoft.com/demo106/yomz-storefront-home/",
+};
+
+for (const [id, url] of Object.entries(links)) {
+    const link = document.getElementById(id);
+    if (link) {
+        link.href = url;
+    }
+} 
